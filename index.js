8f8af4a72c82f99759aa16d6d008b9cdccabcdaa
enrique21 <j.enrique4791@gmail.com>
2020-04-21 11:40:08 -0500

    Agregando 3DPlus completo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-var visualization = d3plus.viz()
+var visualization_1 = d3plus.viz()
 .container("#viz1")
 .data([{"year": 1991, "name":"alpha", "value": 15},
 {"year": 1992, "name":"alpha", "value": 20},])
@@ -9,4 +9,65 @@ var visualization = d3plus.viz()
 .id("name")
 .x("year")
 .y("value")
+.draw()
+
+var data_2 = [
+    {"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1991, "name":"beta", "value": 10},
+    {"year": 1991, "name":"gamma", "value": 5},
+    {"year": 1991, "name":"delta", "value": 50},
+    {"year": 1992, "name":"alpha", "value": 20},
+    {"year": 1992, "name":"beta", "value": 10},
+    {"year": 1992, "name":"gamma", "value": 10},
+    {"year": 1992, "name":"delta", "value": 43},
+    {"year": 1993, "name":"alpha", "value": 30},
+    {"year": 1993, "name":"beta", "value": 40},
+    {"year": 1993, "name":"gamma", "value": 20},
+    {"year": 1993, "name":"delta", "value": 17},
+    {"year": 1994, "name":"alpha", "value": 60},
+    {"year": 1994, "name":"beta", "value": 60},
+    {"year": 1994, "name":"gamma", "value": 25},
+    {"year": 1994, "name":"delta", "value": 32}
+  ]
+
+var visualization_2 = d3plus.viz()
+.container("#viz2")
+.data(data_2)
+.type("bar")
+.id("name")
+.x("year")
+.y("value")
+.draw()
+
+var data_3 = [
+    {"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1992, "name":"alpha", "value": 34},
+    {"year": 1991, "name":"alpha2", "value": 17},
+    {"year": 1992, "name":"alpha2", "value": 65},
+    {"year": 1991, "name":"beta", "value": 10},
+    {"year": 1992, "name":"beta", "value": 10},
+    {"year": 1991, "name":"beta2", "value": 40},
+    {"year": 1992, "name":"beta2", "value": 38},
+    {"year": 1991, "name":"gamma", "value": 5},
+    {"year": 1992, "name":"gamma", "value": 10},
+    {"year": 1991, "name":"gamma2", "value": 20},
+    {"year": 1992, "name":"gamma2", "value": 34},
+    {"year": 1991, "name":"delta", "value": 50},
+    {"year": 1992, "name":"delta", "value": 43},
+    {"year": 1991, "name":"delta2", "value": 17},
+    {"year": 1992, "name":"delta2", "value": 35}
+  ]
+
+var visualization_3 = d3plus.viz()
+.container("#viz3")
+.data(data_3)
+.type("box")
+.id("name")
+.x("year")
+.y("value")
+.ui([{
+  "label": "Tipo de Visualizacion",
+  "method": "type", 
+  "value": ["scatter","box"]
+}])
 .draw()
